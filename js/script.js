f3e67254a3671058e67ec49053119143ce2c2572
Diana Diaz <93556710+WinterLanterns@users.noreply.github.com>
2022-08-31 14:58:55 -0700

    finally finished darkmode

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 // dark mode
 function darkM() {
-  var darkMode = document.querySelector("section");
+  let darkMode = document.querySelector("section");
   darkMode.classList.toggle("dark-mode");
-  var lightOn = document.querySelector(".light-bulb");
+  let lightOn = document.querySelector(".light-bulb");
   lightOn.classList.toggle("bulb-off");
-  var tvImg = document.querySelector(".tv-img");
+  let tvImg = document.querySelector(".tv-img");
   tvImg.classList.toggle("tv-black");
+  let questText = document.querySelector("textarea");
+  questText.classList.toggle("questbText");
+  let questImg = document.querySelector(".tape");
+  questImg.classList.toggle("tapeDark");
+  let enterB = document.querySelector(".enterButton");
+  enterB.classList.toggle("enterDbutton");
 }

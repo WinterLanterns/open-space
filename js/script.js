40f806cd7f0f2b73b4a65e01c9cccc1186db8cc8
Diana Diaz <93556710+WinterLanterns@users.noreply.github.com>
2022-08-31 19:52:00 -0700

    finally finished darkmode

--- js/script.js
+++ js/script.js
@@ -13,3 +13,59 @@ function darkM() {
   let enterB = document.querySelector(".enterButton");
   enterB.classList.toggle("enterDbutton");
 }
+
+// Question & Answers
+function randomAnswers(max_number) {
+  return Math.round(Math.random() * max_number);
+}
+let button = document.querySelector(".enterButton");
+let answer = document.querySelector(".ansText");
+
+button.addEventListener("click", function () {
+  let randomNum = randomAnswers(19);
+  let answerResult = "";
+
+  if (randomNum == 0) {
+    answerResult = "My reply is NO!";
+  } else if (randomNum == 1) {
+    answerResult = "Don't count on it";
+  } else if (randomNum == 2) {
+    answerResult = "My sources say no";
+  } else if (randomNum == 3) {
+    answerResult = "Very doubtful";
+  } else if (randomNum == 4) {
+    answerResult = "Outlook not so good";
+  } else if (randomNum == 5) {
+    answerResult = "Better not tell you now";
+  } else if (randomNum == 6) {
+    answerResult = "Cannot predict now";
+  } else if (randomNum == 7) {
+    answerResult = "Ask again later";
+  } else if (randomNum == 8) {
+    answerResult = "Reply hazy try again";
+  } else if (randomNum == 9) {
+    answerResult = "Concentrate and ask again";
+  } else if (randomNum == 10) {
+    answerResult = "Outlook good";
+  } else if (randomNum == 11) {
+    answerResult = "Most likely";
+  } else if (randomNum == 12) {
+    answerResult = "Signs point to YES!";
+  } else if (randomNum == 13) {
+    answerResult = "As I see it, yes";
+  } else if (randomNum == 14) {
+    answerResult = "YES!";
+  } else if (randomNum == 15) {
+    answerResult = "You may rely on it";
+  } else if (randomNum == 16) {
+    answerResult = "Without a doubt";
+  } else if (randomNum == 17) {
+    answerResult = "Yes, definitely";
+  } else if (randomNum == 19) {
+    answerResult = "It is decidedly so";
+  } else {
+    answerResult = "It is certain";
+  }
+
+  answer.innerHTML = answerResult;
+});
